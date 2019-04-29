0942470e6adfecddfa75493f91ea26fb6981afd6
unikz force <h.bakhtiary.z@gmail.com>
2019-04-29 20:04:44 +0430

    first Contact with kubernetes own api

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+import io.kubernetes.client.ApiClient;
+import io.kubernetes.client.ApiException;
+import io.kubernetes.client.Configuration;
+import io.kubernetes.client.apis.CoreV1Api;
+import io.kubernetes.client.models.V1Pod;
+import io.kubernetes.client.models.V1PodList;
+import io.kubernetes.client.util.Config;
+
+public class Main {
+	public static void main(String[] args) throws IOException, ApiException {
+		ApiClient  client = Config.defaultClient();
+		Configuration.setDefaultApiClient(client);
+		
+		CoreV1Api api = new CoreV1Api();
+		V1PodList list =
+				api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+		
+	    for (V1Pod item : list.getItems()) {
+	        System.out.println(item.getMetadata().getName());
+	    }
+	}
+}
